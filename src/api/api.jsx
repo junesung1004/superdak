441aab29663f88962180255dc649df3150d4fdce
junesung1004 <junecoding1004@gmail.com>
2024-05-14 16:17:34 +0900

    api key값 --> .env-local파일 생성 후 따로 보관하기

---
+++ src/api/api.jsx
@@ -0,0 +1,11 @@
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCZ2I-Airu0G7y-U9XB8PRd7IrSETOEm9E",
+  authDomain: "superdak-f9e5b.firebaseapp.com",
+  projectId: "superdak-f9e5b",
+  // storageBucket: "superdak-f9e5b.appspot.com",
+  // messagingSenderId: "147630142018",
+  // appId: "1:147630142018:web:2a4018fc9c322ab014b3db",
+  // measurementId: "G-400CYKP69L"
+}
