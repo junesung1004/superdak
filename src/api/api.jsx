487157c2d19c1d9a9f0bdbbcafce9482b1cdfef5
junesung1004 <junecoding1004@gmail.com>
2024-05-14 16:21:18 +0900

    api key값 --> .env-local파일 생성 후 따로 보관 후 git에 파일 추가하면 .env.local은 업로드 되지 않음

--- src/api/api.jsx
+++ src/api/api.jsx
@@ -1,11 +1,11 @@
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCZ2I-Airu0G7y-U9XB8PRd7IrSETOEm9E",
-  authDomain: "superdak-f9e5b.firebaseapp.com",
-  projectId: "superdak-f9e5b",
+  apiKey: NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: NEXT_PUBLIC_FIREBASE_PROJECT_ID,
   // storageBucket: "superdak-f9e5b.appspot.com",
   // messagingSenderId: "147630142018",
   // appId: "1:147630142018:web:2a4018fc9c322ab014b3db",
   // measurementId: "G-400CYKP69L"
-}
+};
