4c82a76ef5cb7108241ec52811eefe8d1c3e8670
junesung1004 <junecoding1004@gmail.com>
2024-06-03 10:06:00 +0900

    반응형 수정

--- src/components/TopDownButtonStyle.jsx
+++ src/components/TopDownButtonStyle.jsx
@@ -4,7 +4,7 @@ export const TopDownButtonContainer = styled.div`
   display: flex;
   flex-direction: column;
   gap: 20px;
-  padding: 20px;
+  padding: 10px;
   border: 1px solid #9e9c9c;
   border-radius: 10px;
   width: 100px;
@@ -13,7 +13,7 @@ export const TopDownButtonContainer = styled.div`
   background-color: transparent;
   z-index: 33;
   top: 400px;
-  right: 30px;
+  right: 12px;
 
   .logo-wrap {
     display: flex;
