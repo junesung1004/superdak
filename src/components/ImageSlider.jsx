9472173fff937aa658f87b7545216020d44f300e
junesung1004 <junecoding1004@gmail.com>
2025-03-07 10:00:04 +0900

    fix: 배너 수정

--- src/components/ImageSlider.jsx
+++ src/components/ImageSlider.jsx
@@ -1,41 +1,61 @@
-
-import { Autoplay, EffectFade, Navigation, Pagination, Scrollbar } from "swiper/modules"
-import { Swiper ,SwiperSlide } from "swiper/react"
-import 'swiper/css'
-import 'swiper/css/effect-fade'
-import 'swiper/css/pagination';
-import 'swiper/css/scrollbar';
-import 'swiper/css/navigation';
-import {SwiperContainer} from './ImageSliderStyle'
-
+import {
+  Autoplay,
+  EffectFade,
+  Navigation,
+  Pagination,
+  Scrollbar,
+} from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/effect-fade";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
+import "swiper/css/navigation";
+import { SwiperContainer } from "./ImageSliderStyle";
 
 export default function ImageSlider() {
   return (
-    <SwiperContainer  className="slide-container">
+    <SwiperContainer className="slide-container">
       <Swiper
-      loop={true}
-      slidesPerView= {1}
-      autoplay={{delay: 1500}}
-      speed={3000}
-      navigation
-      modules={[Navigation, Autoplay, EffectFade, Pagination, Scrollbar]}
-      effect = "fade"
-      scrollbar={{ draggable: true }}
-      pagination={{ clickable: true }}
+        loop={true}
+        slidesPerView={1}
+        autoplay={{ delay: 1500 }}
+        speed={3000}
+        navigation
+        modules={[Navigation, Autoplay, EffectFade, Pagination, Scrollbar]}
+        effect="fade"
+        scrollbar={{ draggable: true }}
+        pagination={{ clickable: true }}
       >
         <SwiperSlide>
-          <img src="main-image-1.gif" alt="메인 슬라이더 이미지1" />
+          <img
+            src="main-image-1.gif"
+            alt="메인 슬라이더 이미지1"
+            style={{ width: "100%" }}
+          />
         </SwiperSlide>
         <SwiperSlide>
-          <img src="main-image-2.jpg" alt="메인 슬라이더 이미지2" />
+          <img
+            src="main-image-2.jpg"
+            alt="메인 슬라이더 이미지2"
+            style={{ width: "100%" }}
+          />
         </SwiperSlide>
         <SwiperSlide>
-          <img src="main-image-3.jpg" alt="메인 슬라이더 이미지3" />
+          <img
+            src="main-image-3.jpg"
+            alt="메인 슬라이더 이미지3"
+            style={{ width: "100%" }}
+          />
         </SwiperSlide>
         <SwiperSlide>
-          <img src="main-image-4.jpg" alt="메인 슬라이더 이미지4" />
+          <img
+            src="main-image-4.jpg"
+            alt="메인 슬라이더 이미지4"
+            style={{ width: "100%" }}
+          />
         </SwiperSlide>
       </Swiper>
     </SwiperContainer>
-  )
+  );
 }
