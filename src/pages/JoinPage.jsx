d867149f92c016d7ba096eaee4756b2af6cf60ff
junesung1004 <junecoding1004@gmail.com>
2025-03-07 10:10:45 +0900

    fix: 회원가입 이메일 로직 수정

--- src/pages/JoinPage.jsx
+++ src/pages/JoinPage.jsx
@@ -28,7 +28,8 @@ export default function JoinPage() {
     //console.log("email : ", e.target.value)
 
     //이메일 정규 표현식 (유효성 검사 코드 = validation)
-    const regex = /^(([^<>()\[\].,;:\s@"]+(\.[^<>()\[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/i;
+    const regex =
+      /^(([^<>()\[\].,;:\s@"]+(\.[^<>()\[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/i;
     if (regex.test(email)) {
       setEmailValid(true);
     } else {
@@ -42,7 +43,8 @@ export default function JoinPage() {
     //console.log("password : ", e.target.value)
 
     //비밀번호 정규 표현식 (유효성 검사 코드 = validation)
-    const regex = /^(?=.*[a-zA-z])(?=.*[0-9])(?=.*[$`~!@$!%*#^?&\\(\\)\-_=+])(?!.*[^a-zA-z0-9$`~!@$!%*#^?&\\(\\)\-_=+]).{8,20}$/;
+    const regex =
+      /^(?=.*[a-zA-z])(?=.*[0-9])(?=.*[$`~!@$!%*#^?&\\(\\)\-_=+])(?!.*[^a-zA-z0-9$`~!@$!%*#^?&\\(\\)\-_=+]).{8,20}$/;
     if (regex.test(password)) {
       setPasswordValid(true);
     } else {
@@ -94,39 +96,31 @@ export default function JoinPage() {
     }
   };
 
-  const clickCheckEmail = async () => {
-    try {
-      const emailRef = databaseRef(database, "users");
-      const snapshot = await get(emailRef);
-      if (snapshot.exists()) {
-        const users = snapshot.val();
-        console.log("users : ", users);
-        alert("중복된 이메일입니다. 이메일을 바꿔주세요.");
-      } else {
-        alert("가입이 가능한 이메일입니다.");
-      }
-    } catch (err) {
-      console.log("이메일 중복 체크 기능 에러 : ", err);
-    }
-  };
+  // const clickCheckEmail = async () => {
+  //   try {
+  //     const snapshot = await get();
+  //   } catch (err) {
+  //     console.error("이메일 중복 체크 기능 에러 : ", err);
+  //   }
+  // };
 
-  //중복된 이메일 체크버튼 disabled 활성화 조건
-  useEffect(() => {
-    if (emailValid) {
-      setCheckNotAllow(false);
-      return;
-    }
-    setCheckNotAllow(true);
-  }, [emailValid]);
+  // //중복된 이메일 체크버튼 disabled 활성화 조건
+  // useEffect(() => {
+  //   if (emailValid) {
+  //     setCheckNotAllow(false);
+  //     return;
+  //   }
+  //   setCheckNotAllow(true);
+  // }, [emailValid]);
 
   //emailvalid 와 passwordvalid가 마운트될때 버튼 활성화
   useEffect(() => {
-    if (emailValid && passwordValid && confirmPwValid && nameValid) {
+    if (passwordValid && confirmPwValid && nameValid) {
       setNotAllow(false);
       return;
     }
     setNotAllow(true);
-  }, [emailValid, passwordValid, confirmPwValid, nameValid]);
+  }, [passwordValid, confirmPwValid, nameValid]);
 
   return (
     <JoinContainer>
@@ -141,47 +135,99 @@ export default function JoinPage() {
           <label htmlFor="email">이메일 주소</label>
           <div className="input-wrap">
             <FaUser />
-            <input id={"email"} type="text" name="email" placeholder="test@gmail.com" value={email} onChange={handleEmailChange} />
-            <button className="check-btn" type="button" disabled={notCheckAllow} onClick={clickCheckEmail}>
+            <input
+              id={"email"}
+              type="text"
+              name="email"
+              placeholder="test@gmail.com"
+              value={email}
+              onChange={handleEmailChange}
+            />
+            {/* <button
+              className="check-btn"
+              type="button"
+              disabled={notCheckAllow}
+              onClick={clickCheckEmail}
+            >
               중복확인
-            </button>
+            </button> */}
           </div>
         </div>
-        <div className="error-message-wrap">{!emailValid && email.length > 0 && <div>올바른 이메일을 입력해주세요.</div>}</div>
+        <div className="error-message-wrap">
+          {!emailValid && email.length > 0 && (
+            <div>올바른 이메일을 입력해주세요.</div>
+          )}
+        </div>
 
         {/* 비밀번호 입력창 */}
         <div className="input-title">
           <label htmlFor="password">비밀번호</label>
           <div className="input-wrap">
             <RiLockPasswordLine />
-            <input id={"password"} type="password" name="password" placeholder="영문, 숫자, 특수문자 포함 8자 이상" value={password} onChange={handlePasswordChange} />
+            <input
+              id={"password"}
+              type="password"
+              name="password"
+              placeholder="영문, 숫자, 특수문자 포함 8자 이상"
+              value={password}
+              onChange={handlePasswordChange}
+            />
           </div>
         </div>
-        <div className="error-message-wrap">{!passwordValid && password.length > 0 && <div>영문, 숫자, 특수문자 포함 8자 이상 입력해주세요.</div>}</div>
+        <div className="error-message-wrap">
+          {!passwordValid && password.length > 0 && (
+            <div>영문, 숫자, 특수문자 포함 8자 이상 입력해주세요.</div>
+          )}
+        </div>
 
         {/* 비밀번호 재입력 확인창 */}
         <div className="input-title">
           <label htmlFor="confirmPw">비밀번호 확인</label>
           <div className="input-wrap">
             <RiLockPasswordLine />
-            <input id={"confirmPw"} type="password" name="confirmPw" placeholder="비밀번호를 한번더 입력해주세요." value={confirmPw} onChange={handleConfirmPassword} />
+            <input
+              id={"confirmPw"}
+              type="password"
+              name="confirmPw"
+              placeholder="비밀번호를 한번더 입력해주세요."
+              value={confirmPw}
+              onChange={handleConfirmPassword}
+            />
           </div>
         </div>
-        <div className="error-message-wrap">{!confirmPwValid && confirmPw.length > 0 && <div>비밀번호가 일치하지 않습니다.</div>}</div>
+        <div className="error-message-wrap">
+          {!confirmPwValid && confirmPw.length > 0 && (
+            <div>비밀번호가 일치하지 않습니다.</div>
+          )}
+        </div>
 
         {/* 이름 입력창 */}
         <div className="input-title">
           <label htmlFor="name">이름</label>
           <div className="input-wrap">
             <FaUser />
-            <input id={"name"} type="text" name="name" placeholder="이름을 입력해주세요." value={name} onChange={handleNameChange} />
+            <input
+              id={"name"}
+              type="text"
+              name="name"
+              placeholder="이름을 입력해주세요."
+              value={name}
+              onChange={handleNameChange}
+            />
           </div>
         </div>
-        <div className="error-message-wrap">{!nameValid && name.length > 0 && <div>이름을 입력해주세요.</div>}</div>
+        <div className="error-message-wrap">
+          {!nameValid && name.length > 0 && <div>이름을 입력해주세요.</div>}
+        </div>
 
         {/* 이메일, 비밀번호, 닉네임 입력 후 활성화 되는 버튼 */}
         <ButtonWrap>
-          <button type="button" disabled={notAllow} className="join-button" onClick={handleJoinEvent}>
+          <button
+            type="button"
+            disabled={notAllow}
+            className="join-button"
+            onClick={handleJoinEvent}
+          >
             가입하기
           </button>
 
